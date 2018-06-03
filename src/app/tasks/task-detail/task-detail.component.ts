63503806878f096b49c650545057c2d3858e8239
marcoguilherme <mgbm13@gmail.com>
2018-06-03 19:35:54 -0300

    Cleaning project code

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
 
 import { switchMap } from 'rxjs/operators';
@@ -13,7 +13,7 @@ import { TaskService } from '../shared/task.service';
 
 export class TaskDetailComponent implements OnInit{
 
-    @Input() public task: Task;
+    public task: Task;
 
     public constructor(
         private taskService: TaskService,
