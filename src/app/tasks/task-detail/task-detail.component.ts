c51f8bd6e049eb466447b51e3b5f7be7d466a7a1
marcoguilherme <mgbm13@gmail.com>
2018-06-03 02:56:16 -0300

    New component created to task detail

---
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { Task } from '../shared/task.model';
+
+@Component({
+    selector: 'task-detail',
+    templateUrl: './task-detail.component.html'
+})
+
+export class TaskDetailComponent {}
