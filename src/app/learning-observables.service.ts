027a55bbadc075a8718093e2f9a7390315ba33f1
marcoguilherme <mgbm13@gmail.com>
2018-06-04 02:59:40 -0300

    Learning observables

--- src/app/learning-observables.service.ts
+++ src/app/learning-observables.service.ts
@@ -1,5 +1,8 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable()
 
@@ -7,14 +10,20 @@ export class LearningObservablesService {
 
     public constructor(private http: Http){
         let observer = {
-            name: "some name",
-            email: "some email",
+            next: newData => 
+               console.log("Next method invoked and recieved", newData),
+            error: errorData =>
+                console.log("error method invoked and recieved: ", errorData),
+            complete: () =>console.log("complete  method invoked and finished"),
+        };
 
-            nameAndEmail: function(){
-                return this.name + " and " + this.email
-            }
-        }
+        //Creating observervable object setting a observer at parameter
+        this.http.get("api/tasks").pipe(
+            catchError( this.handleErrors )
+        ).subscribe( observer );
+    }
 
-        console.log(observer.nameAndEmail())
+    public handleErrors(error: Response){
+        return Observable.throw(error)
     }
 }
